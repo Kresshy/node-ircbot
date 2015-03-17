cb8f8a1b986465cc36bd211e3c6cc5a74dc2dc2c
Szabolcs Varadi <szabolcs_varadi@epam.com>
2015-03-17 18:07:25 +0100

    adding async lib for paralell queries

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,20 +3,55 @@
  */
 
 var express = require('express');
+var async = require('async');
 var LogModel = require('../../models/log');
+var ChannelModel = require('../../models/channel');
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
 
-    LogModel.find().limit(40).exec(function(err, logs) {
+    var data = {};
 
-        var data = {
-            _logs: logs
-        };
+    function logQuery(cb) {
+        LogModel.find().limit(40).exec(function (err, logs) {
+
+            if (err) {
+                cb(err, null);
+            }
+
+            cb(null, logs);
+        });
+    }
+
+    function channelQuery(cb) {
+        ChannelModel.find().exec(function(err, channels) {
+
+            if(err) {
+                cb(err, null);
+            }
+
+            cb(null, channels);
+        });
+    }
+
+    async.parallel([
+        logQuery,
+        channelQuery
+    ], complete);
+
+    function complete(err, results) {
+
+        if(err) {
+            next(err);
+        }
+
+        data._logs = results[0];
+        data._channels = results[1];
 
         res.render('index', data);
-    });
+    }
 });
 
 module.exports = router;
